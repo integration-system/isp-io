709f981c6e0e7cb29a3d11b5922e6353ec20ed7b
ykomissarov <merser1q@gmail.com>
2019-06-06 18:53:45 +0300

    add pipe utility

--- pipe.go
+++ pipe.go
@@ -4,10 +4,12 @@ import (
 	"io"
 )
 
-type Pipe []io.Writer
+type Pipe struct {
+	writers []io.Writer
+}
 
 func (p Pipe) Close() error {
-	for _, w := range p {
+	for _, w := range p.writers {
 		if f, ok := w.(interface{ Flush() error }); ok {
 			f.Flush() //TODO handle error
 		}
@@ -19,25 +21,24 @@ func (p Pipe) Close() error {
 }
 
 func (p Pipe) Write(bytes []byte) (int, error) {
-	if len(p) == 0 {
+	if len(p.writers) == 0 {
 		return 0, nil
 	}
-	return p[0].Write(bytes)
+	return p.writers[0].Write(bytes)
 }
 
 func (p *Pipe) Unshift(wr io.Writer) {
-	pipe := append(Pipe{wr}, *p...)
-	p = &pipe
+	p.writers = append([]io.Writer{wr}, p.writers...)
 }
 
 func (p Pipe) Last() io.Writer {
-	if len(p) == 0 {
+	if len(p.writers) == 0 {
 		return nil
 	}
 
-	return p[len(p)-1]
+	return p.writers[len(p.writers)-1]
 }
 
-func NewPipe() Pipe {
+func NewPipe(writers ...io.Writer) Pipe {
 	return Pipe{}
 }
